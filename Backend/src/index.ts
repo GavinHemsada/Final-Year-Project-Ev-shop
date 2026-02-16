7d6f4f31ff32b0d8c070d0c3feaa445e875fb0f5
gavin hemsada <69586429+GavinHemsada@users.noreply.github.com>
2026-02-16 21:28:17 +0530

    fix port number

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -7,7 +7,7 @@ import DB from "./config/DBConnection";
 import logger from "./shared/utils/logger";
 import { connectRedis } from "./config/redis";
 
-const PORT = process.env.PORT;
+const PORT = Number(process.env.PORT);
 const start = async () => {
   try {
     // Connect to MongoDB
@@ -17,7 +17,7 @@ const start = async () => {
     await connectRedis();
     logger.info('Redis connected successfully');
     // Start the server
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on ${process.env.CLIENT_URL}:${PORT}`);
       logger.info(`Server running on ${process.env.CLIENT_URL}:${PORT}`);
     });
